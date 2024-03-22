907b9a86c1634683b6c29397f1b18063ab4aa94d
Filip Novakov <filip.novakov13@gmail.com>
2024-03-22 12:40:01 +0100

    Added necessary methods for LoginController in the UserService,used ErrorDTO's for error messages

--- src/main/java/com/blue/foxbuy/controllers/LoginController.java
+++ src/main/java/com/blue/foxbuy/controllers/LoginController.java
@@ -1,5 +1,9 @@
 package com.blue.foxbuy.controllers;
 
+import com.blue.foxbuy.models.DTOs.ErrorDTO;
+import com.blue.foxbuy.models.DTOs.UserDTO;
+import com.blue.foxbuy.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,17 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 
+    private final UserService userService;
+
+    @Autowired
+    public LoginController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserDTO userDTO){
         if (userDTO.getUsername().isEmpty() || userDTO.getPassword().isEmpty()){
-            return ResponseEntity.badRequest().body("Empty field/s");
+            return ResponseEntity.badRequest().body(new ErrorDTO("Empty field/s"));
         } else if (userService.findByUsername(userDTO.getUsername()) == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Username not found");
-        } else if (userService.findByUsername(userDTO.getUsername()).isEmailVerified == false) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                    "Email not verified, please check your spam folder or click the 'Resend email verification' button");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorDTO("Username not found"));
+        } else if (!userService.findByUsername(userDTO.getUsername()).isEmailVerified()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorDTO(
+                    "Email not verified, please check your spam folder or click the 'Resend email verification' button"));
         } else if (userService.findByUsernameAndPassword(userDTO) == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Password or Username incorrect");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorDTO("Password or Username incorrect"));
         }
 
         //String jwtToken = generateJwtToken(userDTO.getUsername());
