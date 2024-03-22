ba0cb5e4c03ffc7e7f8e3672817ee4a0cf486e8e
Arthur Poghosyan <arthurpoghosyan@gmail.com>
2024-03-22 09:02:08 +0100

    Triple tested the EmailService to send simple and HTML type messages with Mailtrap.

--- src/main/java/com/blue/foxbuy/controllers/EmailVerificationRestController.java
+++ src/main/java/com/blue/foxbuy/controllers/EmailVerificationRestController.java
@@ -1,33 +1,54 @@
 package com.blue.foxbuy.controllers;
 
-package com.blue.foxbuy.services.UserService;
+//package com.blue.foxbuy.services.UserService;
 
+import com.blue.foxbuy.services.EmailService;
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmailVerificationRestController {
-    private final UserRepository userRepository;
+
+    private final EmailService emailService;
 
     @Autowired
-    public EmailVerificationRestController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public EmailVerificationRestController(EmailService emailService) {
+        this.emailService = emailService;
     }
 
-    @GetMapping("/verify-email")
-    public ResponseEntity<String> verifyEmail(@RequestParam("token") String token) {
-        User user = userRepository.findByEmailVerificationToken(token);
+//    private final UserRepository userRepository;
+//
+//    @Autowired
+//    public EmailVerificationRestController(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+//    @GetMapping("/verify-email")
+//    public ResponseEntity<String> verifyEmail(@RequestParam("token") String token) {
+//        User user = userRepository.findByEmailVerificationToken(token);
+//
+//        if (user != null) {
+//            user.setEmailVerified(true);
+//            userRepository.save(user);
+//            return ResponseEntity.ok("Email verified successfully");
+//        } else {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid verification token");
+//        }
+//    }
 
-        if (user != null) {
-            user.setEmailVerified(true);
-            userRepository.save(user);
-            return ResponseEntity.ok("Email verified successfully");
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid verification token");
+    @RequestMapping("/test")
+    public String sendEmailTest() {
+        try {
+            emailService.sendEmail("arthurpoghosyan@gmail.com", "This is a test", "<h1>Hello World!</h1>");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
         }
+        return "E-mail test message successfully sent to arthurpoghosyan@gmail.com!";
     }
 }
--- src/main/java/com/blue/foxbuy/services/EmailService.java
+++ src/main/java/com/blue/foxbuy/services/EmailService.java
@@ -3,36 +3,67 @@ package com.blue.foxbuy.services;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-    private final JavaMailSender javaMailSender;
+
+    // For our purposes we are going to use Java Mail Sender to send e-mails
+    // It automatically connects to the application.properties file and
+    // derives the required web mail set up information, like smtp url,
+    // username and password
+
+    private JavaMailSender mailSender;
 
     @Autowired
-    public EmailService(JavaMailSender javaMailSender) {
-        this.javaMailSender = javaMailSender;
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
     }
 
+    // Here we use the MimeMessage in combination with MimeMessageHelper
+    // to compose our message.
+    //
+    // "MIME" stands for Multipurpose Internet Mail Extensions. It is an
+    // Internet standard that extends the format of email messages to
+    // support text in character sets other than ASCII, as well as attachments
+    // of audio, video, images, and application programs.
+    //
+    // We can send messages without MimeMessageHelper
+    // but it is really cumbersome, look:
+    //
+    // mimeMessage.setFrom(new InternetAddress("from@example.com"));
+    // mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress("to@example.com"));
+    // mimeMessage.setSubject("Your Subject");
+    // mimeMessage.setContent("<h1>Hello</h1><p>This is an HTML email.</p>", "text/html");
+    //
+    // mailSender.send(mimeMessage);
+    //
+    // Now compare it to the MimeMessageHelper:
+
     public void sendEmail(String to, String subject, String htmlContent) throws MessagingException {
-        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(htmlContent, true);
-        javaMailSender.send(message);
+
+        mailSender.send(message);
     }
-//  There is also the possibility of sending simple text as the e-mail
-//  body, so we should experiment and see, which works best
-//    public void sendEmail(String to, String subject, String text) {
+
+//  We can also send messages using SimpleMailMessage, but not HTMLs
+//  or messages that contain various attachments. This would be nice
+//  if we were trying to send really simple messages. Hence, the name.
+//    public void sendEmail(String to, String subject, String body) {
 //        SimpleMailMessage message = new SimpleMailMessage();
+//
+//        message.setFrom("example@example.com");
 //        message.setTo(to);
 //        message.setSubject(subject);
-//        message.setText(text);
-//        javaMailSender.send(message);
+//        message.setText(body);
+//
+//        mailSender.send(message);
 //    }
 }
-
-
