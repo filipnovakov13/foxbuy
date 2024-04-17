22981e14662553489aeb4def81758ef385549e9d
Arthur Poghosyan <arthurpoghosyan@gmail.com>
2024-04-17 23:18:39 +0200

    Fixed an issue with JwtValidationFilter causing problems as an argument for the newly included UserServices was missing

--- src/main/java/com/blue/foxbuy/config/SecurityConfig.java
+++ src/main/java/com/blue/foxbuy/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.blue.foxbuy.config;
 
 import com.blue.foxbuy.filters.JwtValidationFilter;
+import com.blue.foxbuy.services.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,12 +16,17 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 
 @Configuration
 public class SecurityConfig {
+    private final UserService userService;
+
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
-                .addFilterBefore(new JwtValidationFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(jwtValidationFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/registration",
                                 "/login",
@@ -37,6 +43,11 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public JwtValidationFilter jwtValidationFilter() {
+        return new JwtValidationFilter(userService);
     }
 
     @Bean
