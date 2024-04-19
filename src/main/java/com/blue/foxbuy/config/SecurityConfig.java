25a4f75153eec1fe51111cd9d1254a8bd3118ae6
Arthur Poghosyan <arthurpoghosyan@gmail.com>
2024-04-19 16:24:16 +0200

    Cleaned up SecurityConfig and added descriptions for clarity

--- src/main/java/com/blue/foxbuy/config/SecurityConfig.java
+++ src/main/java/com/blue/foxbuy/config/SecurityConfig.java
@@ -3,12 +3,10 @@ package com.blue.foxbuy.config;
 import com.blue.foxbuy.filters.JwtValidationFilter;
 import com.blue.foxbuy.filters.LoggingFilter;
 import com.blue.foxbuy.repositories.LogRepository;
-import jakarta.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.blue.foxbuy.repositories.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,7 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 @Configuration
 public class SecurityConfig {
