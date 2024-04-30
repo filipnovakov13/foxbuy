65621abd45961629f19d3144c6b840c4b9e2c649
Filip Novakov <filip.novakov13@gmail.com>
2024-04-30 21:11:47 +0200

    SecurityConfig changes

--- src/main/java/com/blue/foxbuy/config/SecurityConfig.java
+++ src/main/java/com/blue/foxbuy/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.blue.foxbuy.repositories.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
